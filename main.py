efd454f77743647c71a28f2b3f386a3a8e56ead0
shruthi253 <shruthi253@gmail.com>
2023-07-06 00:58:49 +0200

    added test cases and changed project structure

---
+++ main.py
@@ -0,0 +1,66 @@
+from sync_folders import sync
+from sync_folders import cli
+import os
+import time
+import sys
+import tomllib
+from pprint import pprint
+
+
+def sync_folders(source_path, replica_path, sync_interval, log_file_path):
+    # Synchronize folders periodically
+    while True:
+        try:
+            sync.synchronize(source_path, replica_path, log_file_path)
+            time.sleep(sync_interval)
+
+        except Exception as e:
+            print("Error occurred during synchronization:", str(e))
+            sync.log_operation(log_file_path, "Error occurred during synchronization: " + str(e))
+
+
+def check_same_path(source, replica):
+    if source == replica:
+        print("Source and replica cannot be same path", file = sys.stderr)
+        sys.exit(1)
+
+
+def source_path_exists(source):
+    # check if source exists
+    if not os.path.exists(source):
+        print("Source path does not exist.", file = sys.stderr)
+        sys.exit(1)
+
+
+def create_replica(replica):
+    # Create replica folder if it does not exist
+    if not os.path.exists(replica):
+        os.makedirs(replica)
+
+
+def call_cli_arguments():
+    # Passing commandline arguments
+    args = cli.cli_arguments()
+
+    # Ensure source and replica folder paths are valid
+    source_path  = os.path.abspath(args.source_path)
+    replica_path = os.path.abspath(args.replica_path)
+
+    check_same_path(source_path, replica_path)
+    source_path_exists(source_path)
+    create_replica(replica_path)
+
+    # call sync function
+    sync_folders(source_path, replica_path, args.sync_interval, args.log_file_path)
+
+
+def load_toml() -> dict:    
+    # Load TOML data from a file
+    with open('.\\config.toml', 'rb' ) as cf :
+        toml_data: dict = tomllib.load(cf)
+        return toml_data    
+
+if __name__ == '__main__':
+    data : dict = load_toml()
+    pprint(data, sort_dicts= False)
+    call_cli_arguments()  
---
+++ tests/test_main.py
@@ -0,0 +1,46 @@
+from main import check_same_path,source_path_exists,create_replica
+from sync_folders import cli
+from unittest.mock import patch
+import pytest
+import os
+
+
+def test_check_same_path(capsys, tmpdir):
+    # Prepare test case data
+    source_path = str(tmpdir.join("source"))
+    replica_path = str(tmpdir.join("source"))
+
+    # Call the function
+    with pytest.raises(SystemExit) as exc_info:
+        check_same_path(source_path, replica_path)
+
+    # Assert the output and exit code
+    out, err = capsys.readouterr()
+    assert "Source and replica cannot be same path" in err
+    assert out == ""
+    assert exc_info.type == SystemExit
+    assert exc_info.value.code == 1
+
+
+def test_source_path_exists(capsys, tmpdir):
+    # Prepare test case data
+    source_path = str("")
+
+    # Call the function
+    with pytest.raises(SystemExit) as exc_info:
+        source_path_exists(source_path)
+
+    # Assert the output and exit code
+    out, err = capsys.readouterr()
+    assert "Source path does not exist." in err
+    assert out == ""
+    assert exc_info.type == SystemExit
+    assert exc_info.value.code == 1
+
+
+
+def test_create_replica(tmpdir):
+    replica_folder = str(tmpdir.join("replica"))
+    create_replica(replica_folder)
+
+    assert os.path.exists(os.path.join(replica_folder))
